fa0063553fa880225ab1ab31c014507ed510dd41
Charlie Mills <charlie@computerbacon.com>
2013-11-29 17:56:53 +0000

    All models added to admin.py

--- sandwich/admin.py
+++ sandwich/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from sandwich.models import Sandwich, Retailer, Rating
+
+admin.site.register(Sandwich)
+admin.site.register(Retailer)
+admin.site.register(Rating)
