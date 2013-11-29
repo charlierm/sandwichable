9ff4a27aa29754ac7cbaed391a7667f6478425b7
Charlie Mills <charlie@computerbacon.com>
2013-11-29 17:57:20 +0000

    Added Rating, Tag and helper class to models.py

--- sandwich/models.py
+++ sandwich/models.py
@@ -5,11 +5,28 @@ import uuid
 
 # Create your models here.
 
+class ModelUtils(object):
+    """
+    A big fucking sack of static helper methods
+    """
+
+    @staticmethod
+    def get_image_path(instance, filename):
+        """
+        Returns a filepath for the tasty image to be uploaded to
+        """
+        name, ext = os.path.splitext(filename)
+        filename = "{0}.{1}".format(uuid.uuid4(), ext)
+        return os.path.join(settings.MEDIA_ROOT, 'images', filename)
+
 class Retailer(models.Model):
     """
     Lots of money sapping pricks.
     """
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Sandwich(models.Model):
@@ -18,4 +35,32 @@ class Sandwich(models.Model):
     """
     name = models.CharField(max_length=200)
     barcode = models.CharField(max_length=30, blank=True)
-    image = models.ImageField(upload_to=os.path.join(settings.MEDIA_ROOT, "images", str(uuid.uuid4())))
+    image = models.ImageField(upload_to=ModelUtils.get_image_path)
+    retailer = models.ForeignKey(Retailer)
+
+    def __str__(self):
+        return "{0}: {1}".format(self.retailer.name, self.name)
+
+
+class Rating(models.Model):
+    """
+    A fokin' individual rating on a bloody festive sandwich.
+    """
+    RATING_CHOICES = [(i, i) for i in range(1, 6)]
+
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    sandwich = models.ForeignKey(Sandwich)
+
+    def __str__(self):
+        return '{0}: {1}'.format(self.sandwich.name[:15], self.rating)
+
+class Tag(models.Model):
+    """
+    Tags for saying what a sandwich has in it, e.g. this sandwich has a fuckton
+    of turkey.
+    """
+    name = models.CharField(max_length=20)
+    sandwich = models.ForeignKey(Sandwich)
+
+    def __str__(self):
+        return self.name
